e19e60942bae7560dab7ee2985de9c3911279b48
Tiki9 <isr.ruwantha@gmail.com>
2021-09-04 03:11:15 +0530

    search

--- src/app/front/components/header/header.component.ts
+++ src/app/front/components/header/header.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss','../../../layouts/front-layout/front-layout.component.scss']
+  styleUrls: ['./header.component.scss', '../../../layouts/front-layout/front-layout.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
+  showSearch = false;
   constructor() { }
+
 
   ngOnInit() {
   }
 
+
+  openSearchBox() {
+    this.showSearch = true;
+  }
+  hideSearch() {
+    this.showSearch = false;
+  }
 }
